29153a1bc26d98cdc8ad345e126325d8231eb5ac
Jorge Bussh <jorge.bussh@usach.cl>
2026-02-22 14:40:26 -0300

    wip: migrating to qml

--- gui-desktop/main.cpp
+++ gui-desktop/main.cpp
@@ -6,10 +6,13 @@
 
 int main(int argc, char *argv[])
 {
-    KotobaAppContext appContext;
 
     QApplication a(argc, argv);
-    ThemeManager::setTheme(a, Theme::Dark);  // Set the theme to dark
+    a.setStyle("Fusion");
+    a.setApplicationName("Kotoba+");
+    ThemeManager::setTheme(a, Theme::Dark);  // Set the theme to dark or light
+
+    KotobaAppContext appContext;
     MainWindow w(&appContext);
     w.show();
     return a.exec();
